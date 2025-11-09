28a3d511cc3742240ef6ac602d7edf2b1138f271
Abdulatif <reply.791u0@slmails.com>
2025-11-10 00:15:09 +0500

    Comment

--- internal/database/queries/users.go
+++ internal/database/queries/users.go
@@ -6,6 +6,7 @@ import (
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
+// CreateUser handler for creating user
 func CreateUser(db *pgxpool.Pool, username, email, password string) error {
 	_, err := db.Exec(context.Background(), `
 			INSERT INTO users (username, email, password)
